b1955d6ac99e332e26869f4c14ca6d3fedbd5a53
JPedroc17 <jpccarvalho2210@gmail.com>
2023-01-17 14:56:14 -0300

    fixed error in arguments

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jocardos <jocardos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 19:16:22 by jocardos          #+#    #+#             */
-/*   Updated: 2023/01/11 15:56:07 by jocardos         ###   ########.fr       */
+/*   Updated: 2023/01/17 14:50:56 by jocardos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	main(int argc, char **argv)
 	t_const_philo	var;
 	int				init;
 
-	init = init_all(&var, argv);
 	if (argc != 5 && argc != 6)
 		return (print_error("Wrong amount of arguments"));
+	init = init_all(&var, argv);
 	if (init)
 		return (manager_error(init));
 	if (create_routine(&var))
